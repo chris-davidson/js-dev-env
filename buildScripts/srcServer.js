0b816fff5ca5bca7e2e200b74d07ccf23ba81877
Chris Davidson <chrisdavidson.net@gmail.com>
2019-04-15 10:22:13 -0400

    mock api with user data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -23,6 +23,16 @@ app.get('/', function (req, res){
   res.sendfile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+    debugger;  // eslint-disable-line no-debugger
+    // Hard coding for simplicity.  Pretend this hits a real database
+    res.json([
+        {'id': 1, 'firstName': 'Bob', 'lastName': 'Smith', 'email': 'bob@email.com'},
+        {'id': 2, 'firstName': 'Tammy', 'lastName': 'Norton', 'email': 'tnorton@yahoo.com'},
+        {'id': 3, 'firstName': 'Tina', 'lastName': 'Lee', 'email': 'lee.tina@hotmail.com'}
+    ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
